13683c3ee6e99db55700b3b7376fb82237cdaa7d
lenalesnaya <lenalesnaya1993@gmail.com>
2023-07-08 21:24:50 +0300

    Removed allure attributes from api tests.

--- TestMonitorTesting/Tests/API/ProjectTests.cs
+++ TestMonitorTesting/Tests/API/ProjectTests.cs
@@ -20,10 +20,6 @@ namespace TestMonitorTesting.Tests.API
         }
 
         [Test, Category("Positive"), Description("Adding of a project with random values.")]
-        [AllureSeverity(SeverityLevel.critical)]
-        [AllureOwner("Any user")]
-        [AllureSuite("API")]
-        [AllureSubSuite("ProjectAPITests")]
         [SmokeTest]
         public void AddProject()
         {
@@ -39,10 +35,6 @@ namespace TestMonitorTesting.Tests.API
         }
 
         [Test, Category("Positive"), Description("Getting of recently added project.")]
-        [AllureSeverity(SeverityLevel.critical)]
-        [AllureOwner("Any user")]
-        [AllureSuite("API")]
-        [AllureSubSuite("ProjectAPITests")]
         [SmokeTest]
         public void GetProject()
         {
@@ -59,10 +51,6 @@ namespace TestMonitorTesting.Tests.API
         }
 
         [Test, Category("Negative"), Description("Getting of an unexisted project.")]
-        [AllureSeverity(SeverityLevel.normal)]
-        [AllureOwner("Any user")]
-        [AllureSuite("API")]
-        [AllureSubSuite("ProjectAPITests")]
         [Regression]
         public void GetUnexistedProject()
         {
